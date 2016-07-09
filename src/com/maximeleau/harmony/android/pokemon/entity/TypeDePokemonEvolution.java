742e8d109de84aa6fff999299ffbf5f656fe0c33
Maxime LEAU <maximeleau@wanadoo.fr>
2016-07-09 23:34:36 +0200

    Fix entity n-n relation.

--- src/com/maximeleau/harmony/android/pokemon/entity/TypeDePokemonEvolution.java
+++ src/com/maximeleau/harmony/android/pokemon/entity/TypeDePokemonEvolution.java
@@ -2,18 +2,24 @@ package com.maximeleau.harmony.android.pokemon.entity;
 
 import com.tactfactory.harmony.annotation.Column;
 import com.tactfactory.harmony.annotation.Entity;
+import com.tactfactory.harmony.annotation.GeneratedValue;
 import com.tactfactory.harmony.annotation.Id;
 import com.tactfactory.harmony.annotation.ManyToOne;
+import com.tactfactory.harmony.annotation.Column.Type;
+import com.tactfactory.harmony.annotation.GeneratedValue.Strategy;
 
 @Entity
 public class TypeDePokemonEvolution {
 		
 	@Id
+    @Column(type = Type.INTEGER, hidden = true)
+    @GeneratedValue(strategy = Strategy.MODE_IDENTITY)
+	private int id;
+	
 	@ManyToOne(targetEntity="TypeDePokemon")
 	@Column(nullable = false)
 	private TypeDePokemon evolueEn;
-	
-	@Id
+
 	@ManyToOne(targetEntity="TypeDePokemon")
 	@Column(nullable = false)
 	private TypeDePokemon estEvolueEn;
