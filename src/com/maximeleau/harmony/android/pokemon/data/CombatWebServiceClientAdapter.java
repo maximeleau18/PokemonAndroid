e2fa239ba21309bd1addb9a5be4bd09c3bc25838
Maxime LEAU <maximeleau@wanadoo.fr>
2016-08-05 16:13:49 +0200

    Add connexion dresseur for dresseur in webservice. Add searchfight for combat in webservice.

--- src/com/maximeleau/harmony/android/pokemon/data/CombatWebServiceClientAdapter.java
+++ src/com/maximeleau/harmony/android/pokemon/data/CombatWebServiceClientAdapter.java
@@ -11,9 +11,13 @@
 package com.maximeleau.harmony.android.pokemon.data;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.maximeleau.harmony.android.pokemon.data.base.CombatWebServiceClientAdapterBase;
 import com.maximeleau.harmony.android.pokemon.entity.Combat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Rest class for {@link Combat} WebServiceClient adapters.
@@ -79,5 +83,38 @@ public class CombatWebServiceClientAdapter
     public CombatWebServiceClientAdapter(Context context,
             String host, Integer port, String scheme, String prefix) {
         super(context, host, port, scheme, prefix);
+
+
+    }
+
+    /**
+     * Retrieve one Combat. Uses the route : Combat/searchemptycombat.
+     * @param combat : The Combat to retrieve (set pokemon2 and dresseur2)
+     * @return -1 if an error has occurred. 0 if not.
+     */
+    public int searchEmptyFight(Combat combat) {
+        int result = -1;
+        String response = this.invokeRequest(
+                RestClient.Verb.POST,
+                String.format(
+                        this.getUri() + "/searchemptycombat",
+                        combat.getDresseur2().getId(),
+                        combat.getPokemon2().getId(),
+                        REST_FORMAT),
+                itemToJson(combat));
+
+        if (this.isValidResponse(response) && this.isValidRequest()) {
+            try {
+                JSONObject json = new JSONObject(response);
+                if (extract(json, combat)) {
+                    result = 0;
+                }
+            } catch (JSONException e) {
+                Log.e(TAG, e.getMessage());
+                combat = null;
+            }
+        }
+
+        return result;
     }
 }
--- src/com/maximeleau/harmony/android/pokemon/data/DresseurWebServiceClientAdapter.java
+++ src/com/maximeleau/harmony/android/pokemon/data/DresseurWebServiceClientAdapter.java
@@ -11,9 +11,13 @@
 package com.maximeleau.harmony.android.pokemon.data;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.maximeleau.harmony.android.pokemon.data.base.DresseurWebServiceClientAdapterBase;
 import com.maximeleau.harmony.android.pokemon.entity.Dresseur;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Rest class for {@link Dresseur} WebServiceClient adapters.
@@ -79,5 +83,36 @@ public class DresseurWebServiceClientAdapter
     public DresseurWebServiceClientAdapter(Context context,
             String host, Integer port, String scheme, String prefix) {
         super(context, host, port, scheme, prefix);
+    }
+
+    /**
+     * Retrieve one Dresseur. Uses the route : Dresseur/connexion.
+     * @param dresseur : The Dresseur to retrieve (set the login and password)
+     * @return -1 if an error has occurred. 0 if not.
+     */
+    public int connectDresseur(Dresseur dresseur) {
+        int result = -1;
+        String response = this.invokeRequest(
+                RestClient.Verb.POST,
+                String.format(
+                        this.getUri() + "/connexion",
+                        dresseur.getLogin(),
+                        dresseur.getMotDePasse(),
+                        REST_FORMAT),
+                itemToJson(dresseur));
+
+        if (this.isValidResponse(response) && this.isValidRequest()) {
+            try {
+                JSONObject json = new JSONObject(response);
+                if (extract(json, dresseur)) {
+                    result = 0;
+                }
+            } catch (JSONException e) {
+                Log.e(TAG, e.getMessage());
+                dresseur = null;
+            }
+        }
+
+        return result;
     }
 }
